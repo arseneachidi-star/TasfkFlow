9a38571af0ba03eca51037cdfb7fd757cdd2e8ed
Arsène Atchidi <arseneachidi@gmail.com>
2026-08-06 18:24:23 +0200

    Ajout de la fonction de configuration de l'adminiostrateur

--- public/index.php
+++ public/index.php
@@ -4,7 +4,19 @@
  * Rôle : Front Controller unique de l'application.
  */
 
+
+
 session_start();
+
+
+
+// Inclusion des classes/contrôleurs
+require_once __DIR__ . '/../Controllers/AdminController.php'; 
+// (Adapte le chemin '../app/Controllers/' selon l'arborescence réelle de ton projet)
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
 
 // Nettoyage et découpage de l'URL
 $url = $_GET['url'] ?? '';
@@ -37,4 +49,18 @@ if (file_exists($controllerFile)) {
 } else {
     http_response_code(404);
     echo "Erreur 404 : Contrôleur introuvable.";
+
+// Le routage de l'administrateur 
+
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// Nettoyage éventuel du préfixe du sous-dossier XAMPP si nécessaire
+$uri = str_replace('/taskflow/public', '', $uri); // Adapte selon ton arborescence
+
+if ($uri === '/admin/dashboard') {
+    $controller = new AdminController();
+    $controller->dashboard();
+} elseif (preg_match('#^/admin/task/delete/(\d+)$#', $uri, $matches)) {
+    $controller = new AdminController();
+    $controller->deleteTask($matches[1]);
+}
 }
